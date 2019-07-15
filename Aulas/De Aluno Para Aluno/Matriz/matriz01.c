cde19d03adca7bbe1be24dbf4dd73c1b2562b2a6
Douglas Tott <douglasifbaianosb@gmail.com>
2019-07-15 13:51:37 -0300

    Aula de C

--- Aulas/De Aluno Para Aluno/Matriz/matriz01.c
+++ Aulas/De Aluno Para Aluno/Matriz/matriz01.c
@@ -6,9 +6,19 @@ int main(void) {
                         {4,5,6},
                         {7,8,9}};
 
+    int matriz2[3][3] = {1,2,3,4,5,6,7,8,9};
+
     for(int i = 0; i < 3; i++) {
         for(int l = 0; l < 3; l++) {
             printf("\n%i", matriz[i][l]);
+        }
+    }
+
+    printf("\n\n");
+
+    for(int j = 0; j < 3; j++) {
+        for(int k = 0; k < 3; k++) {
+            printf("\n%i", matriz2[j][k]);
         }
     }
 
