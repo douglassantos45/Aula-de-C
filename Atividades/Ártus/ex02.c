03768b7ae57b4d76f72b8b2a38cdc52b1ae119c9
Douglas Tott <julhomenes@gmail.com>
2019-10-14 12:13:26 -0300

    Atividade do Ártus

--- Atividades/Ártus/ex02.c
+++ Atividades/Ártus/ex02.c
@@ -1,14 +1,50 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include"pilha.h"
+
+#define MAX_SIZE 10
+#define TRUE  1
+#define FALSE 0
 
 /*Dada uma seqüência de operações push e pop e de um inteiro representando o tamanho de um vetor no qual uma pilha deve ser implementada, elabore um algoritmo para determinar a ocorrência ou não de estouro. O algoritmo não deverá usar uma pilha. Implemente o algoritmo como um programa em C.*/
 
+typedef struct { 
+    int vector[MAX_SIZE];
+    int totalE;
+
+} VECTOR;
+
+void initVector(VECTOR *v) {
+    v->totalE = 0;
+}
+
+int overFlow(VECTOR *v) {
+    return(v->totalE == MAX_SIZE);
+}
+
+void push(VECTOR *v, int element) {
+    if(overFlow(v) == TRUE){
+        printf("ERR: VECTOR IS FULL!!\n");
+    } else {
+        v->vector[v->totalE] = element;
+        v->totalE++;
+    }
+}
 
 int main(int argc, char const *argv[]) {
-    /* code */
 
-    
+    VECTOR v;
+
+    initVector(&v);
+
+    for(int i = 0; i < MAX_SIZE; i++) { 
+        push(&v, i * 10 + 1);
+    }
+
+    for(int i = 0; i < MAX_SIZE; i++) {
+        printf("%d\n", v.vector[i]);
+    }
+
+    push(&v, 30);
 
     return 0;
 }
---
+++ Atividades/Ártus/ex05.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+
+#define MAX_SIZE 20
+#define True  1
+#define False 0
+/*Elabore um método para manter duas pilhas dentro de um único vetor linear de modo que nenhuma das pilhas incorra em estouro até que toda a momória seja usada, e uma pilha inteira nunca seja deslocada para outro local dentro do vetor. Escreva rotinas em C, push1, push2, pop1 e pop2, para manipular as duas pilhas. (Dica: as duas pilhas crescem na diração da outra)*/
+
+typedef struct {
+    int array[MAX_SIZE];
+    int top1;
+    int top2;
+}DUBLESTACK;
+
+void initDubleStack(DUBLESTACK *ds) {
+    ds->top1 = -1;
+    ds->top2 = MAX_SIZE;
+}
+
+int sizeDubleStack(DUBLESTACK *ds) {
+    return ds->top1 + 1 + MAX_SIZE - ds->top2;
+}
+
+int overflow(DUBLESTACK *ds) {
+    if(ds->top1 + 1 == ds->top2) return False;
+}
+
+int underflow(DUBLESTACK *ds) {
+    if(ds->top1 == -1 || ds->top2 == MAX_SIZE) return False;
+}
+
+int insert(DUBLESTACK *ds, int stack, int value) {
+    if(overflow(ds) == False) {
+        printf("ERR: ");
+    } else {
+        if(stack < 1 || stack > 2) return False;
+        
+        if(stack == 1) {
+            ds->array[ds->top1++] = value;
+        } else {
+            ds->array[ds->top2--] = value;
+        }
+    }
+}
+
+int remove(DUBLESTACK *ds, int stack) {
+    if(underflow(ds) == False) {
+        printf("ERR: ");
+    } else {
+        if(stack < 1 || stack > 2) return False;
+
+        if(stack == 1) ds->top1--;
+        else ds->top2++;    
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
